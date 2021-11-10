372edb346ddcb6b1499bce5c898e4495ddf3182f
Shohmasud <name@mail>
2021-11-11 04:26:58 +0700

    settings.py:Сorrection Syntax error in the 40 field

--- place_remember/settings.py
+++ place_remember/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'remember.apps.RememberConfig'
+    'remember.apps.RememberConfig',
     'django.contrib.sites',
     'allauth',
     'allauth.account',
