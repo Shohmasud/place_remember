b87eb13c3ae0bdb9a34f7085eefb78612eea5774
Shohmasud <name@mail>
2021-11-11 06:47:43 +0700

    models.py:Creating an address storage field in the RememberPlaceFB class

--- remember/models.py
+++ remember/models.py
@@ -2,3 +2,4 @@ from django.db import models
 
 # Create your models here.
 class RememberPlaceFB(models.Model):
+    place = models.CharField(verbose_name='Address Facebook', max_length=5000, db_index=True, blank=False, unique=True)
