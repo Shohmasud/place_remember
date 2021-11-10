874fa6f442a2ebcfd42b6bb2e9deec61f53b7c33
Shohmasud <name@mail>
2021-11-11 06:55:11 +0700

    models.py:Meta class that shows the name of the table in the database in the admin panel

--- remember/models.py
+++ remember/models.py
@@ -4,3 +4,7 @@ from django.db import models
 class RememberPlaceFB(models.Model):
     place = models.CharField(verbose_name='Address Facebook', max_length=5000, db_index=True, blank=False, unique=True)
     comment = models.CharField(verbose_name='Comment Facebook', max_length=5000, db_index=True, blank=False,unique=False)
+
+    class Meta:
+        verbose_name = 'Remember_Place Facebook-[Autofill]'
+        ordering = ['id']
