5d18e89709a8dda6fd6e17a8f0997940350def39
Shohmasud <name@mail>
2021-11-11 10:54:48 +0700

    view.py:Import all models(UserLogFB,RememberPlaceFB,RememberPlaceVk,UserLogVk) ,for the application to work

--- remember/views.py
+++ remember/views.py
@@ -1,3 +1,6 @@
+from remember.models import UserLogFB,RememberPlaceFB,RememberPlaceVk,UserLogVk
+from django.contrib.auth.models import User
+from django.views.generic import View
 from django.shortcuts import render
-
 # Create your views here.
+
