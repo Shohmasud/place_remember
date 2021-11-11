376104bd4f224dd3f27b5478e888e30aa8ec2373
Shohmasud <name@mail>
2021-11-11 11:39:00 +0700

    place_remember/urls.py:Comment-hint,examples of a request to the server

--- remember/urls.py
+++ remember/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path,include
 from remember.views import LoginView
 urlpatterns = [
+    #http://localhost:8000/path/
     path('admin/', admin.site.urls),
     path(r'^accounts/',include('allauth.urls')),
     path('login/', LoginView.as_view()),
