cb34ba4588bce8b052a95c2ec9bccb4909343fc9
Shohmasud <name@mail>
2021-11-11 17:23:19 +0700

    views.py:Import class-form RememberForm from remember.forms

--- remember/views.py
+++ remember/views.py
@@ -2,6 +2,7 @@ from remember.models import UserLogFB,RememberPlaceFB,RememberPlaceVk,UserLogVk
 from django.contrib.auth.models import User
 from django.views.generic import View
 from django.shortcuts import render
+from remember.forms import RememberForm
 # Create your views here.
 
 
