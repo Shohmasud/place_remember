d16c55a510688c3318f0e57281676dd225ad8403
Shohmasud <name@mail>
2021-11-11 23:39:36 +0700

    Composing the url path for the FormPostView class, which is responsible for rendering the template sign.html

--- remember/urls.py
+++ remember/urls.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from django.urls import path,include
-from remember.views import LoginView,SignView
+from remember.views import LoginView,SignView,FormPostView
 urlpatterns = [
     #http://localhost:8000/path/
     path('admin/', admin.site.urls),
     path(r'^accounts/',include('allauth.urls')),
     path('login/', LoginView.as_view()),
-    path('sign/social/',SignView.as_view(),name='sign')
+    path('sign/social/',SignView.as_view(),name='sign'),
+    path('post/form/<str:pk>/', FormPostView.as_view(), name='post_form_facebook'),
 ]
