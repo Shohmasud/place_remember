85bd2f4d5a8a2b3150db09868e03dfc6e6d9f65f
Shohmasud <name@mail>
2021-11-12 01:54:43 +0700

    tests/test_view.py:Import of all models and modules for working with tests in django

--- remember/tests/test_view.py
+++ remember/tests/test_view.py
@@ -1,2 +1,9 @@
+from remember.models import UserLogFB,UserLogVk,RememberPlaceVk,RememberPlaceFB
+from django.test import TestCase
+from remember.forms import RememberForm
+from django.contrib.auth.models import User
+import re
 
+from django.test import Client
+csrf_client = Client(enforce_csrf_checks=True)
 
