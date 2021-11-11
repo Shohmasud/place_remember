844fa9ba69b804be517c64fb53ee4c171532a941
Shohmasud <name@mail>
2021-11-11 08:32:59 +0700

    admin.py:Creating the Admin_UserLogVk class for displaying models (tables) of the database given in the admin panel

--- remember/admin.py
+++ remember/admin.py
@@ -11,3 +11,9 @@ class Admin_RememberPlaceVK(admin.ModelAdmin):
     list_display = ('id', 'place','comment')
     search_fields = ['id','place','comment']
 admin.site.register(RememberPlaceVk,Admin_RememberPlaceVK)
+
+class Admin_UserVk(admin.ModelAdmin):
+    list_display = ('id', 'user')
+    search_fields = ['id','user']
+admin.site.register(UserLogVk,Admin_UserVk)
+
