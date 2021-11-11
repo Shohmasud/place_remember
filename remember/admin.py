cfaa92aabc1b3563388b1082e1b870b31272b65c
Shohmasud <name@mail>
2021-11-11 08:22:07 +0700

    admin.py:Creating the Admin_RememberPlaceFB class for displaying models (tables) of the database given in the admin panel

--- remember/admin.py
+++ remember/admin.py
@@ -2,3 +2,7 @@ from django.contrib import admin
 from .models import RememberPlaceFB,UserLogFB,UserLogVk,RememberPlaceVk
 
 # Register your models here.
+class Admin_RememberPlaceFB(admin.ModelAdmin):
+    list_display = ('id', 'place','comment')
+    search_fields = ['id','place','comment']
+admin.site.register(RememberPlaceFB,Admin_RememberPlaceFB)
