738a8496211a63e5c68ac4db6727bd8b92bc833a
Shohmasud <name@mail>
2021-11-11 16:44:24 +0700

    views.py:Create the class responsible for the output of the sign.html file

--- remember/views.py
+++ remember/views.py
@@ -9,3 +9,7 @@ class LoginView(View):
     """The class responsible for the output of the login.html file"""
     def get(self, request):
         return render(request, 'login.html')
+
+
+class SignView(View):
+    """The class responsible for the output of the sign.html file"""
