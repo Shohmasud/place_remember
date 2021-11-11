4274de23cb035c8ba3f6cb71a2ab92722bb8ea16
Shohmasud <name@mail>
2021-11-11 17:09:41 +0700

    Creating fields (location,text_comment) for working with the form and receiving data from the user

--- remember/forms.py
+++ remember/forms.py
@@ -3,4 +3,12 @@ from remember.models import RememberPlaceFB
 
 
 class RememberForm(forms.Form):
+    location = forms.CharField(label='Location', max_length=5000, widget=forms.TextInput(
+        attrs={'rows': '1', 'cols': '5', 'class': 'form-control',
+               'placeholder': 'Location...'},
+    ))
+    text_comment = forms.CharField(label='Comment', max_length=5000, widget=forms.Textarea(
+        attrs={'rows': '5', 'cols': '5', 'class': 'form-control',
+               'placeholder': 'Say Something...'},
+    ))
 
