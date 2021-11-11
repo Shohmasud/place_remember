111c319dde9b098f538795d81e47d81d3ee076e8
Shohmasud <name@mail>
2021-11-11 19:32:42 +0700

    remember/urls.py:Setting urls path for template output sign.html

--- remember/urls.py
+++ remember/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path,include
-from remember.views import LoginView
+from remember.views import LoginView,SignView
 urlpatterns = [
     #http://localhost:8000/path/
     path('admin/', admin.site.urls),
     path(r'^accounts/',include('allauth.urls')),
     path('login/', LoginView.as_view()),
+    path('sign/social/',SignView.as_view(),name='sign')
 ]
