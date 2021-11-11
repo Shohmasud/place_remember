e42d747861da5f13c46d991919ebf3a9f35748d7
Shohmasud <name@mail>
2021-11-11 19:23:26 +0700

    filters_tags_templatetags:Creating a custom filter to check the existence of a user in the database and the existence of addresses associated with a given user user and output

--- remember/templatetags/filters_tags_templatetags.py
+++ remember/templatetags/filters_tags_templatetags.py
@@ -6,3 +6,40 @@ register = template.Library()
 
 users_modelAll = [UserLogFB,UserLogVk]
 remembers_modelsAll = [RememberPlaceVk,RememberPlaceFB]
+#
+@register.filter
+def verify_user(value):
+    global first_name
+    first_name = value
+    for elements in users_modelAll:
+        try:
+            fullname = elements.objects.get(user=value)
+            list_releted = [res for data in elements.objects.all() if data.user == value for res in data.releted_place]
+            len_change_list = len(list(set(list_releted)))
+
+            if len_change_list == 0:
+                return "You don't have any memories"
+            return "Add more memories"
+
+        except:
+            provider = None
+            for obj in User.objects.all():
+                if obj.first_name == value:
+                    provider = obj.username
+            matches = re.findall('id[1-9]*', provider)
+
+            if len(matches) == 0:
+                try:
+                    create_user = UserLogFB.objects.create(user=value)
+                    create_user.save()
+                    return "You don't have any memories"
+                except:
+                    return "You don't have any memories"
+
+            try:
+                create_user = UserLogVk.objects.create(user=value)
+                create_user.save()
+                return "You don't have any memories"
+            except:
+                return "You don't have any memories"
+
